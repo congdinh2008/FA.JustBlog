cba64b1448f03ce4c83b716890fefd049eb6b506
Cong Dinh <congdinh2021@gmail.com>
2021-08-30 15:12:02 +0700

    Create Project Api Core

---
+++ src/core/FA.JustBlog/FA.JustBlog.API/Controllers/StudentsController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FA.JustBlog.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        public StudentsController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var students = new List<Student>()
+            {
+                new Student()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Student 02",
+                    DateOfBirth = DateTime.Now,
+                },
+                new Student()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Student 03",
+                    DateOfBirth = DateTime.Now,
+                },
+                new Student()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Student 03",
+                    DateOfBirth = DateTime.Now,
+                }
+            };
+            return Ok(students);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        [ProducesResponseType(typeof(Student), 200)]
+        [ProducesResponseType(typeof(Student), 204)]
+        public IActionResult GetById(Guid id)
+        {
+            var students = new List<Student>()
+            {
+                new Student()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Student 02",
+                    DateOfBirth = DateTime.Now,
+                },
+                new Student()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Student 03",
+                    DateOfBirth = DateTime.Now,
+                },
+                new Student()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Student 03",
+                    DateOfBirth = DateTime.Now,
+                }
+            };
+            var student = students.FirstOrDefault(x => x.Id == id);
+            return Ok(student);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Student student)
+        {
+            return Ok(student);
+        }
+    }
+}
---
+++ src/core/FA.JustBlog/FA.JustBlog.API/Student.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace FA.JustBlog.API
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
